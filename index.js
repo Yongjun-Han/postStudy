4d96e85155f0f94f5303124e74ea38e5c172a107
yongjunhan <hanyongjun.dev@gmail.com>
2022-12-01 10:41:26 +0900

    기본 포스트 구현

---
+++ index.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
+const url = require('url');
+const port = 5580;
+
+http.createServer( (req,res)=> {
+  
+      let _url = req.url;
+      let pathname = url.parse(_url, true).pathname;
+      // let urlObject = url.parse(_url, true).query // Object
+      // let urlString = url.parse(_url, false).query // String
+      if(req.method === "GET") {
+          fs.readFile('./index.html' ,'utf8' ,function(error, data) {
+          res.writeHead(200, {'Content-Type' : 'text/html'});
+          res.end(data);
+        });
+      }
+
+      if(req.method === "POST") {
+          fs.readFile('./main.html' ,'utf8' ,function(error, data) {
+          res.writeHead(200, {'Content-Type' : 'text/html'});
+          res.end(data);
+        });
+      }
+      // else if(pathname === '/getlogin') {
+      //   fs.readFile('./index.html' ,'utf8' ,function(error, data) {
+      //     res.writeHead(200, {'Content-Type' : 'text/html'});
+      //     res.end(data);
+      //     // console.dir(pathname);
+      //     console.log(urlObject);
+      //     // console.log(typeof urlString);
+      //     console.log(qs.parse(urlString));
+
+          
+      //     fs.writeFile('./memo.txt', urlObject.id  , (err,data) => {
+      //       if(err) throw err;
+      //     })
+          
+      //   });
+      // }
+      
+      // else if(pathname === '/postlogin') {
+      //   req.on('data', function(chunk) {
+      //       console.log(chunk);
+      //       console.log(chunk.toString());
+      //       let chunkString = chunk.toString();
+
+      //       console.log(qs.parse(chunkString))
+
+      //       let chunkObject = qs.parse(chunkString)
+
+      //       // res.writeHead(200,{'Content-Type' : 'text/html'});
+      //       fs.writeFile('./postmemo.json', JSON.stringify(Object(chunkObject),null) , (err) => {
+      //         if(err) throw err;
+      //       });
+      //       res.end();
+      //     });
+      //   }
+      }).listen(port, function() {
+        console.log('server is running >>>')
+      })
